cc8d97b0972b58041459f44eaf99f7b3c355d4d0
Minnebrieven <libr.libr1711@gmail.com>
2023-06-17 18:38:25 +0700

    [fix] inconsistent route for admin

--- routes/membership_route.go
+++ routes/membership_route.go
@@ -13,13 +13,13 @@ func AddMembershipRoutes(e *echo.Echo) {
 	membershipJWT := e.Group("/memberships")
 	membershipJWT.Use(echojwt.WithConfig(jwtConfig))
 	membershipJWT.GET("/mymembership", controllers.MyMembershipController)
-
-	membershipJWT.GET("", controllers.GetMembershipsController, m.IsAdmin)
-	membershipJWT.GET("/:id", controllers.GetMembershipController, m.IsAdmin)
-	membershipJWT.POST("", controllers.CreateMembershipController, m.IsAdmin)
-	membershipJWT.PUT("/:id", controllers.UpdateMembershipController, m.IsAdmin)
-	membershipJWT.DELETE("/:id", controllers.DeleteMembershipController, m.IsAdmin)
-
 	membershipJWT.POST("/join/:plan_id", controllers.JoinMembershipController)
 
+	adminMembershipJWT := e.Group("/admin/memberships")
+	adminMembershipJWT.Use(echojwt.WithConfig(jwtConfig), m.IsAdmin)
+	adminMembershipJWT.GET("", controllers.GetMembershipsController)
+	adminMembershipJWT.GET("/:id", controllers.GetMembershipController)
+	adminMembershipJWT.POST("", controllers.CreateMembershipController)
+	adminMembershipJWT.PUT("/:id", controllers.UpdateMembershipController)
+	adminMembershipJWT.DELETE("/:id", controllers.DeleteMembershipController)
 }
--- routes/plan_route.go
+++ routes/plan_route.go
@@ -9,10 +9,12 @@ import (
 )
 
 func AddPlanRoutes(e *echo.Echo) {
+	// for users or guests
 	e.GET("/plans/all", controllers.GetPlansController)
 	e.GET("/plans/:id", controllers.GetPlanController)
 
-	planJWT := e.Group("/plans")
+	// for admin
+	planJWT := e.Group("/admin/plans")
 	planJWT.Use(echojwt.WithConfig(jwtConfig), m.IsAdmin)
 	planJWT.POST("", controllers.CreatePlanController)
 	planJWT.PUT("/:id", controllers.UpdatePlanController)
