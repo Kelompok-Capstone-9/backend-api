2bffd4ec71b4b751d853daff2a5548172ff7b463
Minnebrieven <libr.libr1711@gmail.com>
2023-06-17 15:22:55 +0700

    [fix] get all & by id location no longer need auth

--- routes/location_route.go
+++ routes/location_route.go
@@ -9,10 +9,12 @@ import (
 )
 
 func AddLocationRoutes(e *echo.Echo) {
+	e.GET("/locations", controllers.GetLocationsController)
+	e.GET("/locations/:id", controllers.GetLocationByIDController)
+
+	// for administrator
 	locationJWT := e.Group("/admin/locations")
 	locationJWT.Use(echojwt.WithConfig(jwtConfig), m.IsAdmin)
-	locationJWT.GET("/all", controllers.GetLocationsController)
-	locationJWT.GET("/:id", controllers.GetLocationByIDController)
 	locationJWT.POST("", controllers.CreateLocationController)
 	locationJWT.PUT("/:id", controllers.EditLocationController)
 	locationJWT.DELETE("/:id", controllers.DeleteLocationController)
